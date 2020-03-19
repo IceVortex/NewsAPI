42c0a2b0cb0b48200f7918cdd103dcd723fdc438
Mitar Cvjetkovic <mitar-c@hotmail.com>
2020-03-19 12:32:27 +0100

    browsersync done

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
+var connect = require('gulp-connect-php');
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.sass')         //source
+    return gulp.src('./sass/index.sass')
       .pipe(sass())
-      .pipe(gulp.dest('./css'))                 //destination
+      .pipe(gulp.dest('./css'))
       .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
-        },
-        open: 'local',
-        port: 4000
-    });
-
-    gulp.watch('./sass/**/*.sass', gulp.series('sass'));
-    //gulp.watch('./css/**/*.css').on('change', browserSync.reload);  // [Browsersync] File event [change] :
-    gulp.watch('./sass/**/*.sass').on('change', browserSync.reload);  // [Browsersync] Reloading Browsers... (buffered 2 events)
+    connect.server({}, function () {
+        browserSync({
+          proxy: '127.0.0.1:8000'
+        });
+      });
+    gulp.watch('./sass/**/*.sass', ['sass']);
+    gulp.watch('./js/*.js').on('change', browserSync.reload);
+    gulp.watch('./index.php').on('change', browserSync.reload);
 });
+
+gulp.task('default', ['sass','watch']);
--- index.php
+++ index.php
@@ -5,15 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
     <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
     <link rel="stylesheet" type="text/css" href="css/index.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="slick/slick.min.js"></script>
-    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript" src="js/index.js"></script>
     <title>News</title>
 </head>
-
 <body>
     <div class="wrapper">
         <div class="container">
