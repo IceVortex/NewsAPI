3616819017576600c432cc5d0bfe2fc9c810da38
Mitar Cvjetkovic <mitar-c@hotmail.com>
2020-03-26 12:14:50 +0100

    babel and ES6

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var connect = require('gulp-connect-php');
 
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+
 gulp.task('sass', function() {
     return gulp.src('./sass/index.sass')
       .pipe(sass())
@@ -10,13 +17,26 @@ gulp.task('sass', function() {
       .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task('js', function() {
+    return browserify({
+      entries: ['./js/']
+    })
+    .transform( babelify, { presets: ['env']})
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./JSbabel/'))
+});
+
+gulp.task('watch', ['sass', 'js'], function() {
     connect.server({}, function () {
         browserSync({
           proxy: '127.0.0.1:8000'
         });
       });
     gulp.watch('./sass/**/*.sass', ['sass']);
+    gulp.watch('./js/**/*.js', ['js']);
     gulp.watch('./js/*.js').on('change', browserSync.reload);
     gulp.watch('./index.php').on('change', browserSync.reload);
 });
