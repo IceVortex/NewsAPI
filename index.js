69fc987382508a51fb98db7238e5a4405149b893
Mitar Cvjetkovic <mitar-c@hotmail.com>
2020-04-21 11:57:01 +0200

    single page vue

---
+++ index.js
@@ -0,0 +1,31 @@
+new Vue({
+    el: '#root',
+    data: {
+        keywords: '',
+        KEY: 'ffa2a5621d404c09942b8e5767b10bcc',
+        articles: []
+    },
+    methods: {
+        async getArticles() {
+            event.preventDefault();
+            this.articles = [];
+            try {
+                const response = await axios.get('http://newsapi.org/v2/top-headlines', {
+                    params: {
+                        q: this.keywords,
+                        pageSize: 5,
+                        apiKey: this.KEY
+                    }
+                })
+                this.articles = response.data.articles;
+                $('.slidesArray').slick({
+                    prevArrow: "<button id='prevButton'></button>",
+                    nextArrow: "<button id='nextButton'></button>"
+                });
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+    }
+})
